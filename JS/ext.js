6620a5ba385823e198be4f51055b34ac06657297
anshulchaudhary <54776396+anschy@users.noreply.github.com>
2021-06-21 22:15:05 +0530

    Add files via upload

--- JS/ext.js
+++ JS/ext.js
@@ -3,3 +3,10 @@ window.addEventListener("scroll", function() {
 
     header.classList.toggle("sticky", window.scrollY > 0)
 });
+
+function toggleMenu(){
+    var menuToggle = document.querySelector(".toggle");
+    var menu = document.querySelector(".menu");
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+}
